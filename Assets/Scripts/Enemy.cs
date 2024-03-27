ae1aa637e6cda07b4448aefa1a1afc2129e8095a
Brandon Davis <wowzerss90@yahoo.com>
2024-03-27 11:19:35 -0400

    Added enemy prefab and script to move enemy and respawn them back at the top when they go below the screen. Also added trigger event to detect collision and destroy game objects.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    private float _maxX = 8f;
+    private float _minX = -8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+
+        if(transform.position.y <= -6f)
+        {
+            float randX = Random.Range(_minX, _maxX);
+            transform.position = new Vector3(randX, 8f, 0f);
+        } 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit: " + other.tag);
+
+        if(other.tag == "Player")
+        {
+            Debug.Log("Damged player");
+            Destroy(gameObject);
+        } else if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
