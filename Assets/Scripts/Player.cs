630e2329e1f901913e5a55ee47a4c811a4fbb3ed
Brandon Davis <wowzerss90@yahoo.com>
2024-03-27 08:55:16 -0400

    Created Player script that moves player to the right every frame

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * _speed);
+    }
+}
